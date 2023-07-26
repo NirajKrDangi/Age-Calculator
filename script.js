a24d41a0f20ffd3c2bded69830fd383bfc431d2e
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2023-07-26 14:29:48 +0530

    Updated script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+function calculateAge() {
+    // Get the user's date of birth
+    var dob = document.getElementById("dob").value;
+    
+    // Convert the date of birth to a Date object
+    var dobDate = new Date(dob);
+    
+    // Get the current date
+    var currentDate = new Date();
+    
+    // Calculate the age
+    var age = currentDate.getFullYear() - dobDate.getFullYear();
+    
+    // Check if the current month and day are before the birthday
+    if (currentDate.getMonth() < dobDate.getMonth() || 
+        (currentDate.getMonth() === dobDate.getMonth() && currentDate.getDate() < dobDate.getDate())) {
+      age--;
+    }
+    
+    // Display the age
+    document.getElementById("age").innerHTML = age;
+  }
